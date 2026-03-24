8a26f233811d5f1b677592b6faf90a12389e671a
Damglador <52221087+Damglador@users.noreply.github.com>
2026-03-24 21:12:25 +0100

    Update and organize appid_blacklist
    Added new runtimes and sorted entries to be more human-readable

--- src/blacklist.py
+++ src/blacklist.py
@@ -2,23 +2,26 @@
 
 appid_blacklist = [
     "228980",  # Steamworks Common Redistributables
+    "1826330", # Proton EasyAntiCheat Runtime
+    "1161040", # Proton BattlEye Runtime
+    "1070560", # Steam Linux Runtime 1.0 (scout)
+    "1391110", # Steam Linux Runtime 2.0 (soldier)
+    "1628350", # Steam Linux Runtime 3.0 (sniper)
+    "3810310", # Steam Linux Runtime 3.0 - Arm64 (sniper) 
+    "4183110", # Steam Linux Runtime 4.0
+    "4185400", # Steam Linux Runtime 4.0 - Arm64
     "858280",  # Proton 3.7
     "961940",  # Proton 3.16
     "1054830", # Proton 4.2
-    "1070560", # Steam Linux Runtime - Scout
     "1113280", # Proton 4.11
-    "1161040", # Proton BattlEye Runtime
     "1245040", # Proton 5.0
-    "1391110", # Steam Linux Runtime - Soldier
     "1420170", # Proton 5.13
-    "1493710", # Proton Experimental
     "1580130", # Proton 6.3
-    "1628350", # Steam Linux Runtime - Sniper
-    "1826330", # Proton EasyAntiCheat Runtime
     "1887720", # Proton 7.0
-    "2180100", # Proton Hotfix
-    "2230260", # Proton Next
     "2348590", # Proton 8.0
     "2805730", # Proton 9.0
     "3658110", # Proton 10.0
+    "2180100", # Proton Hotfix
+    "2230260", # Proton Next
+    "1493710", # Proton Experimental
 ]
