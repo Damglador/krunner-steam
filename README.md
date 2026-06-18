debe0f8cdf264f2aa7f1a1fe9cbb9d2e18bbd1ad
Damglador <vse.stopchanskyi@gmail.com>
2026-06-18 21:31:05 +0200

    Improve install and update README

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 Unofficial Steam plugin for KRunner.
 
+Actions for search results:
+- Steam Library - opens the game in your Library in Steam
+- Community Hub
+- Local Files
+
+## Installation
+
+### AUR
+
+```sh
+yay -S krunner-steam-git
+```
+
+### Manual
+
+```sh
+cd "${XDG_DATA_HOME:-$HOME/.local/share}/"
+git clone https://github.com/Damglador/krunner-steam
+cd krunner-steam
+./install.sh
+```
+
 ## Screenshot
 
 ![](doc/Screenshot-2022-02-26.png)
--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ prefix="${XDG_DATA_HOME:-$HOME/.local/share}"
 krunner_dbusdir="$prefix/krunner/dbusplugins"
 services_dir="$prefix/dbus-1/services/"
 
-mkdir -p $krunner_dbusdir
-mkdir -p $services_dir
+mkdir -p "$krunner_dbusdir"
+mkdir -p "$services_dir"
 
-cp krunnersteam.desktop $krunner_dbusdir
-printf "[D-BUS Service]\nName=com.github.xtibor.krunnersteam\nExec=\"$PWD/src/main.py\"" > $services_dir/com.github.xtibor.krunnersteam.service
+cp krunner-steam.desktop "$krunner_dbusdir"
+printf '[D-BUS Service]\nName=com.github.xtibor.krunnersteam\nExec=%s/src/main.py' "$PWD" > "$services_dir/com.github.xtibor.krunnersteam.service"
 
 kquitapp6 krunner
